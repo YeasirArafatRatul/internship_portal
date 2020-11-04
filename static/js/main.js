3bd3403c002e12994e3cbacdf94d036bf2de73c9
arafat_ratul <yeasirarafat.ratul@gmail.com>
2020-11-05 01:42:08 +0600

    front end modified

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+(function($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function() {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.featured__controls li').on('click', function() {
+            $('.featured__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.featured__filter').length > 0) {
+            var containerEl = document.querySelector('.featured__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+
+})(jQuery);
